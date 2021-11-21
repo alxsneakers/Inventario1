86c9a6d160baecc3d2013483553c21c19d256d78
Alx <alxsneakers@gmail.com>
2021-11-21 20:24:54 +0100

    V1.5

--- app/src/main/java/com/example/inventario1/VerProductoActivity.java
+++ app/src/main/java/com/example/inventario1/VerProductoActivity.java
@@ -93,7 +93,7 @@ public class VerProductoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(VerProductoActivity.this);
-                builder.setMessage("¿Desea Eliminar este contacto?.")
+                builder.setMessage("¿Desea eliminar este registro?.")
                         .setPositiveButton("SI", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int i) {
